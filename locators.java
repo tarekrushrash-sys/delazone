497485bb262373a678aeb7942295125c378eccdc
tarekrushrash-sys <tarek.rushrash@gmail.com>
2025-09-05 09:58:05 +0300

    Create locators.java

---
+++ locators.java
@@ -0,0 +1,8 @@
+package com.hs.locators;
+
+public class Locators {
+    // Deal Zone Page
+    public static final String DEAL_ZONE_BANNER_ID = "com.hungerstation.android.web:id/deal_zone_image";
+    public static final String CATEGORY_TAB_ID = "com.hungerstation.android.web:id/tabTitle";
+    public static final String PRODUCT_TILE_ID = "com.hungerstation.android.web:id/productTile";
+}
