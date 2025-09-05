760c4f168b87bb55ac4da8288e9d2fe15e7abed3
tarekrushrash-sys <tarek.rushrash@gmail.com>
2025-09-05 09:57:23 +0300

    Create config.java

---
+++ config.java
@@ -0,0 +1,11 @@
+package com.hs.config;
+
+public class Config {
+    // Appium / device configuration
+    public static final String APP_PACKAGE = "com.hungerstation.android";
+    public static final String APP_ACTIVITY = ".MainActivity";
+    public static final String APPIUM_SERVER_URL = "http://127.0.0.1:4723/wd/hub";
+
+    // Timeout
+    public static final int DEFAULT_TIMEOUT = 20;
+}
